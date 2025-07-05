648760c0f82ec9b1fceb463b22e05cb23b8d4dba
nass16 <noaheverly5@gmail.com>
2025-07-05 15:20:56 +0200

    Update signatures.h

--- signatures/signatures.h
+++ signatures/signatures.h
@@ -1,3 +1,3 @@
-dwCreateMove > 48 8B C4 4C 89 40 18 48 89 48 08 55 53 57 41 54 48 8D 68 F8 48 81 EC E8 01 00 00
+dwCreateMove > 48 83 EC 28 48 8B 01 4C 8D 0D ? ? ? ?
 
 leave a star fr this shit took me 2 hours and its currently 02:41am so yall niggas should be grateful
